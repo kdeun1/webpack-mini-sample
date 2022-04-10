30a2ad014fcfa09f334eb688381ed74f3616d517
kimdoeun <kdeun1@ex-em.com>
2022-04-10 20:41:13 +0900

    7. css 설정 (style-loader, css-loader)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { hello, add } from './util';
+import './style.css';
+import './header.css';
 
-const text = hello('안녕! 나는 000이야.');
+const text = hello('<h1>안녕! 나는 000이야.</h1>');
 const sumNum = add(1, 2);
 
 document.getElementById('root').innerHTML = `${text} 결과 값은 ${sumNum}`;
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
